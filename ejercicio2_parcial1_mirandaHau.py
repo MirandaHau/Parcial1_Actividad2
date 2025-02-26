e4eb82bcafc25500d9a3cbf7c6def6a8a71e39e2
Miranda <miranda.hau@anahuac.mx>
2025-02-26 18:32:40 +0100

    Actividad 2

--- ejercicio2_parcial1_mirandaHau.py
+++ ejercicio2_parcial1_mirandaHau.py
@@ -2,10 +2,9 @@ import Crypto.Random
 import Crypto.Util.number
 import hashlib
 
-# Para e vamos a usar el número 4 de Fermat
 e = 65537
 
-# Paso 1: Generación manual de las claves RSA de Alice y la AC
+#Claves RSA de Alice y la AC
 def generate_keypair():
     p = Crypto.Util.number.getPrime(1024, randfunc=Crypto.Random.get_random_bytes)
     q = Crypto.Util.number.getPrime(1024, randfunc=Crypto.Random.get_random_bytes)
@@ -24,7 +23,7 @@ nAc, dAc = generate_keypair()
 print("\nRSA de la AC: n =", nAc)
 print("Clave privada de la AC: d =", dAc)
 
-# Paso 2: Leer el archivo NDA.pdf y calcular el hash
+#Leer el archivo NDA.pdf y calcular el hash
 nda = 'NDA.pdf'
 
 try:
@@ -40,11 +39,11 @@ except FileNotFoundError:
 hM = int.from_bytes(hashlib.sha256(file_content).digest(), byteorder='big')
 print('\nMensaje hasheado:', hex(hM))
 
-# Paso 3: Alice firma el hash con su clave privada
+#Alice firma el hash con su clave privada
 sA = pow(hM, dA, nA)  # Firma el hash de manera manual usando RSA
 print('\nFirma de Alice:', sA)
 
-# Paso 4: La AC verifica la firma de Alice usando la clave pública de Alice
+# La AC verifica la firma de Alice usando la clave pública de Alice
 hM1 = pow(sA, e, nA)  # Verificación usando la fórmula pública
 print('\nLa AC verifica el hash:', hex(hM1))
 
@@ -54,11 +53,11 @@ else:
     print("Error: Los hashes no coinciden.")
     exit(1)
 
-# Paso 5: La AC firma el hash con su clave privada
+#La AC firma el hash con su clave privada
 sAc = pow(hM, dAc, nAc)  # Firma el hash de manera manual usando la clave privada de la AC
 print('\nFirma de la AC:', sAc)
 
-# Paso 6: Bob verifica la firma de la AC usando la clave pública de la AC
+#Bob verifica la firma de la AC usando la clave pública de la AC
 hM2 = pow(sAc, e, nAc)  # Verificación usando la fórmula pública de la AC
 print('\nBob verifica la firma de la AC:', hex(hM2))
 
